af1c316e844b2cd59454d44b1f403cd1f66ce100
Michele Cannito <tigerrosere@gmail.com>
2014-07-07 01:51:35 -0700

    Create Rails Status Application, first commit

---
+++ statbook/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Statbook::Application.config.secret_key_base = 'fac545751cb318dd1d83f2cb7042784d153a2b7a613d8d841e6df165d9151e86a64109c43f33317acd5947c8babe8f6e43e601978c63e25979b622d9b08a9101'
