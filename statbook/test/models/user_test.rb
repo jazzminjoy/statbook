6ad495d934303e171204f6a920c36a4a6e0747c5
Michele Cannito <tigerrosere@gmail.com>
2014-07-18 21:34:36 -0700

    Fixed bug with profile_name validation caused by incorrect regular expression in app/models/user.rb. Modified 'a user should have a profile name without spaces' test and added 'a user can have a correctly formatted profile name' test in test/models/user_test.rb.

--- statbook/test/models/user_test.rb
+++ statbook/test/models/user_test.rb
@@ -7,7 +7,7 @@ class UserTest < ActiveSupport::TestCase
 		assert !user.errors[:first_name].empty?
 	end
 
-	test "a user should enter a last name" do
+	test "a user should enter a last name" do 
 		user = User.new
 		assert !user.save
 		assert !user.errors[:last_name].empty?
@@ -29,11 +29,22 @@ class UserTest < ActiveSupport::TestCase
 	end
 
 	test "a user should have a profile name without spaces" do
-		user = User.new
+		user = User.new(first_name: 'Michele', last_name: 'Buch', email: 'tigerrosere@gmail.com')
+		user.password = user.password_confirmation = 'password1'
+
 		user.profile_name = "My Profile Name With Spaces"
+
 		assert !user.save
 		assert !user.errors[:profile_name].empty?
 		assert user.errors[:profile_name].include?("Must be formatted correctly.")
+	end
+
+	test "a user can have a correctly formatted profile name" do
+		user = User.new(first_name: 'Michele', last_name: 'Buch', email: 'tigerrosere@gmail.com')
+		user.password = user.password_confirmation = 'password1'
+
+		user.profile_name = "Michelle_Buch-1"
+		assert user.valid?
 	end
   # test "the truth" do
   #   assert true
